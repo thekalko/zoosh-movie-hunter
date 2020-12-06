e6c8c4a1adb0ec6f5ba49abe911885410d0dca66
thekalko <thecyberkalko@gmail.com>
2020-12-07 00:24:08 +0100

    :rocket: create schema

--- src/resolvers/queries/userQueries.ts
+++ src/resolvers/queries/userQueries.ts
@@ -1,10 +1,10 @@
 import { prisma } from "prisma";
-import { User } from "@prisma/client";
+// import { User } from "@prisma/client";
 import { ApolloContext } from "src/types/ApolloContext";
 
 
 export default {
-  users: async (_: void, args: void, ctx: ApolloContext): Promise<Array<User>> => {
-    return await prisma.user.findMany();
+  users: async (_: void, args: void, ctx: ApolloContext) => {
+    // return await prisma.user.findMany();
   }
 };
